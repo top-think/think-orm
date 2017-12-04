3f881308a42318ad83c14a24fa330b2cf8c5cc0c
thinkphp <thinkphp@qq.com>
2017-12-04 09:11:20 +0800

    增加whereOr动态查询和unionAll方法

--- src/db/Query.php
+++ src/db/Query.php
@@ -112,6 +112,10 @@ class Query
             // 根据某个字段获取记录的某个值
             $name = Db::parseName(substr($method, 10));
             return $this->where($name, '=', $args[0])->value($args[1]);
+        } elseif (strtolower(substr($method, 0, 7)) == 'whereor') {
+            $name = Loader::parseName(substr($method, 7));
+            array_unshift($args, $name);
+            return call_user_func_array([$this, 'whereOr'], $args);
         } elseif (strtolower(substr($method, 0, 5)) == 'where') {
             $name = Db::parseName(substr($method, 5));
             array_unshift($args, $name);
@@ -725,6 +729,17 @@ class Query
         }
 
         return $this;
+    }
+
+    /**
+     * 查询SQL组装 union all
+     * @access public
+     * @param  mixed   $union
+     * @return $this
+     */
+    public function unionAll($union)
+    {
+        return $this->union($union, true);
     }
 
     /**

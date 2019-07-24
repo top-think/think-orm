c2d0e578c1694bdb66ded74d4244315243b70ab3
thinkphp <thinkphp@qq.com>
2019-07-24 22:49:57 +0800

    修正mysql驱动配置

--- src/db/connector/Mysql.php
+++ src/db/connector/Mysql.php
@@ -27,49 +27,51 @@ class Mysql extends PDOConnection
      */
     protected $config = [
         // 数据库类型
-        'type'            => 'mysql',
+        'type'              => 'mysql',
         // 服务器地址
-        'hostname'        => '127.0.0.1',
+        'hostname'          => '127.0.0.1',
         // 数据库名
-        'database'        => '',
+        'database'          => '',
         // 用户名
-        'username'        => '',
+        'username'          => '',
         // 密码
-        'password'        => '',
+        'password'          => '',
         // 端口
-        'hostport'        => '',
+        'hostport'          => '',
         // 连接dsn
-        'dsn'             => '',
+        'dsn'               => '',
         // 数据库连接参数
-        'params'          => [],
+        'params'            => [],
         // 数据库编码默认采用utf8
-        'charset'         => 'utf8',
+        'charset'           => 'utf8',
         // 数据库表前缀
-        'prefix'          => '',
+        'prefix'            => '',
         // 数据库调试模式
-        'debug'           => false,
+        'debug'             => false,
         // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
-        'deploy'          => 0,
+        'deploy'            => 0,
         // 数据库读写是否分离 主从式有效
-        'rw_separate'     => false,
+        'rw_separate'       => false,
         // 读写分离后 主服务器数量
-        'master_num'      => 1,
+        'master_num'        => 1,
         // 指定从服务器序号
-        'slave_no'        => '',
+        'slave_no'          => '',
         // 模型写入后自动读取主服务器
-        'read_master'     => false,
+        'read_master'       => false,
         // 是否严格检查字段是否存在
-        'fields_strict'   => true,
+        'fields_strict'     => true,
         // 是否需要进行SQL性能分析
-        'sql_explain'     => false,
+        'sql_explain'       => false,
         // Builder类
-        'builder'         => '',
+        'builder'           => '',
         // Query类
-        'query'           => '',
+        'query'             => '',
         // 是否需要断线重连
-        'break_reconnect' => false,
+        'break_reconnect'   => false,
         // 断线标识字符串
-        'break_match_str' => [],
+        'break_match_str'   => [],
+        // 字段缓存路径
+        'schema_cache_path' => '',
     ];
 
     /**

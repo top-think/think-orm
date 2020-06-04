667896c20244894eadd706a7adc3bdca512f7bc7
thinkphp <thinkphp@qq.com>
2020-06-04 15:11:09 +0800

    改进

--- src/db/Builder.php
+++ src/db/Builder.php
@@ -165,7 +165,7 @@ abstract class Builder
                 }
             } elseif (is_null($val)) {
                 $result[$item] = 'NULL';
-            } elseif (is_array($val) && !empty($val)) {
+            } elseif (is_array($val) && !empty($val) && is_string($val[0])) {
                 switch (strtoupper($val[0])) {
                     case 'INC':
                         $result[$item] = $item . ' + ' . floatval($val[1]);

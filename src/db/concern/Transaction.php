35ca511a1e4d671b39f7afb4c887703c16ef6957
thinkphp <thinkphp@qq.com>
2020-06-22 22:57:28 +0800

    还原Query类的事务方法

--- src/db/concern/Transaction.php
+++ src/db/concern/Transaction.php
@@ -71,4 +71,47 @@ trait Transaction
         }
     }
 
+    /**
+     * 执行数据库事务
+     * @access public
+     * @param callable $callback 数据操作方法回调
+     * @return mixed
+     */
+    public function transaction(callable $callback)
+    {
+        return $this->connection->transaction($callback);
+    }
+
+    /**
+     * 启动事务
+     * @access public
+     * @return void
+     */
+    public function startTrans(): void
+    {
+        $this->connection->startTrans();
+    }
+
+    /**
+     * 用于非自动提交状态下面的查询提交
+     * @access public
+     * @return void
+     * @throws PDOException
+     */
+    public function commit(): void
+    {
+        $this->connection->commit();
+    }
+
+    /**
+     * 事务回滚
+     * @access public
+     * @return void
+     * @throws PDOException
+     */
+    public function rollback(): void
+    {
+        $this->connection->rollback();
+    }
+
 }

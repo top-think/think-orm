4a7082a36c910d3ce5c7dea7ec27ac9ee09b4ac1
yunwuxin <448901948@qq.com>
2022-08-11 19:52:41 +0800

    修正

--- src/model/relation/MorphTo.php
+++ src/model/relation/MorphTo.php
@@ -52,11 +52,11 @@ class MorphTo extends Relation
     /**
      * 架构函数
      * @access public
-     * @param  Model  $parent    上级模型对象
-     * @param  string $morphType 多态字段名
-     * @param  string $morphKey  外键名
-     * @param  array  $alias     多态别名定义
-     * @param  ?string $relation  关联名
+     * @param Model $parent 上级模型对象
+     * @param string $morphType 多态字段名
+     * @param string $morphKey 外键名
+     * @param array $alias 多态别名定义
+     * @param  ?string $relation 关联名
      */
     public function __construct(Model $parent, string $morphType, string $morphKey, array $alias = [], string $relation = null)
     {
@@ -110,11 +110,11 @@ class MorphTo extends Relation
     /**
      * 根据关联条件查询当前模型
      * @access public
-     * @param  string  $operator 比较操作符
-     * @param  integer $count    个数
-     * @param  string  $id       关联表的统计字段
-     * @param  string  $joinType JOIN类型
-     * @param  Query   $query    Query对象
+     * @param string $operator 比较操作符
+     * @param integer $count 个数
+     * @param string $id 关联表的统计字段
+     * @param string $joinType JOIN类型
+     * @param Query $query Query对象
      * @return Query
      */
     public function has(string $operator = '>=', int $count = 1, string $id = '*', string $joinType = '', Query $query = null)
@@ -125,10 +125,10 @@ class MorphTo extends Relation
     /**
      * 根据关联条件查询当前模型
      * @access public
-     * @param  mixed  $where 查询条件（数组或者闭包）
-     * @param  mixed  $fields 字段
-     * @param  string $joinType JOIN类型
-     * @param  ?Query  $query    Query对象
+     * @param mixed $where 查询条件（数组或者闭包）
+     * @param mixed $fields 字段
+     * @param string $joinType JOIN类型
+     * @param  ?Query $query Query对象
      * @return Query
      */
     public function hasWhere($where = [], $fields = null, string $joinType = '', Query $query = null)
@@ -139,7 +139,7 @@ class MorphTo extends Relation
     /**
      * 解析模型的完整命名空间
      * @access protected
-     * @param  string $model 模型名（或者完整类名）
+     * @param string $model 模型名（或者完整类名）
      * @return Model
      */
     protected function parseModel(string $model): string
@@ -161,7 +161,7 @@ class MorphTo extends Relation
     /**
      * 设置多态别名
      * @access public
-     * @param  array $alias 别名定义
+     * @param array $alias 别名定义
      * @return $this
      */
     public function setAlias(array $alias)
@@ -184,11 +184,11 @@ class MorphTo extends Relation
     /**
      * 预载入关联查询
      * @access public
-     * @param  array   $resultSet   数据集
-     * @param  string  $relation    当前关联名
-     * @param  array   $subRelation 子关联名
-     * @param  ?Closure $closure     闭包
-     * @param  array   $cache       关联缓存
+     * @param array $resultSet 数据集
+     * @param string $relation 当前关联名
+     * @param array $subRelation 子关联名
+     * @param  ?Closure $closure 闭包
+     * @param array $cache 关联缓存
      * @return void
      * @throws Exception
      */
@@ -214,8 +214,8 @@ class MorphTo extends Relation
                 if (!\is_null($closure)) {
                     $obj = $closure($obj);
                 }
-                $pk    = $obj->getPk();
-                $list  = $obj->with($subRelation)
+                $pk   = $obj->getPk();
+                $list = $obj->with($subRelation)
                     ->cache($cache[0] ?? false, $cache[1] ?? null, $cache[2] ?? null)
                     ->select($val);
                 $data = [];
@@ -245,11 +245,11 @@ class MorphTo extends Relation
     /**
      * 预载入关联查询
      * @access public
-     * @param  Model   $result      数据对象
-     * @param  string  $relation    当前关联名
-     * @param  array   $subRelation 子关联名
-     * @param  ?Closure $closure     闭包
-     * @param  array   $cache       关联缓存
+     * @param Model $result 数据对象
+     * @param string $relation 当前关联名
+     * @param array $subRelation 子关联名
+     * @param  ?Closure $closure 闭包
+     * @param array $cache 关联缓存
      * @return void
      */
     public function eagerlyResult(Model $result, string $relation, array $subRelation = [], Closure $closure = null, array $cache = []): void
@@ -263,23 +263,24 @@ class MorphTo extends Relation
     /**
      * 关联统计
      * @access public
-     * @param  Model   $result  数据对象
+     * @param Model $result 数据对象
      * @param  ?Closure $closure 闭包
-     * @param  string  $aggregate 聚合查询方法
-     * @param  string  $field 字段
+     * @param string $aggregate 聚合查询方法
+     * @param string $field 字段
      * @return integer
      */
     public function relationCount(Model $result, Closure $closure = null, string $aggregate = 'count', string $field = '*')
-    {}
+    {
+    }
 
     /**
      * 多态MorphTo 关联模型预查询
      * @access protected
-     * @param  string $model       关联模型对象
-     * @param  string $relation    关联名
-     * @param  Model  $result
-     * @param  array  $subRelation 子关联
-     * @param  array  $cache       关联缓存
+     * @param string $model 关联模型对象
+     * @param string $relation 关联名
+     * @param Model $result
+     * @param array $subRelation 子关联
+     * @param array $cache 关联缓存
      * @return void
      */
     protected function eagerlyMorphToOne(string $model, string $relation, Model $result, array $subRelation = [], array $cache = []): void
@@ -301,8 +302,8 @@ class MorphTo extends Relation
     /**
      * 添加关联数据
      * @access public
-     * @param  Model  $model  关联模型对象
-     * @param  string $type   多态类型
+     * @param Model $model 关联模型对象
+     * @param string $type 多态类型
      * @return Model
      */
     public function associate(Model $model, string $type = ''): Model
@@ -335,12 +336,10 @@ class MorphTo extends Relation
         return $this->parent->setRelation($this->relation, null);
     }
 
-    protected function buildQuery(Model $model)
+    protected function buildQuery(Query $query)
     {
-        $query = $model->db();
-
         foreach ($this->queryCaller as $caller) {
-            call_user_func_array([$this->query, $caller[0]], $caller[1]);
+            call_user_func_array([$query, $caller[0]], $caller[1]);
         }
 
         return $query;

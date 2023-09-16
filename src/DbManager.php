062f80b544bb9b0e190484109eb8b22651c79bea
thinkphp <thinkphp@qq.com>
2023-09-16 22:40:05 +0800

    优化单独使用orm库的时候可能的内存溢出问题

--- src/DbManager.php
+++ src/DbManager.php
@@ -53,12 +53,6 @@ class DbManager
     protected $listen = [];
 
     /**
-     * SQL日志
-     * @var array
-     */
-    protected $dbLog = [];
-
-    /**
      * 查询次数
      * @var int
      */
@@ -167,8 +161,6 @@ class DbManager
     {
         if ($this->log) {
             $this->log->log($type, $log);
-        } else {
-            $this->dbLog[$type][] = $log;
         }
     }
 
@@ -180,12 +172,7 @@ class DbManager
      */
     public function getDbLog(bool $clear = false): array
     {
-        $logs = $this->dbLog;
-        if ($clear) {
-            $this->dbLog = [];
-        }
-
-        return $logs;
+        return [];
     }
 
     /**
@@ -292,16 +279,17 @@ class DbManager
 
     /**
      * 更新查询次数
+     * @deprecated
      * @access public
      * @return void
      */
     public function updateQueryTimes(): void
     {
-        $this->queryTimes++;
     }
 
     /**
      * 重置查询次数
+     * @deprecated
      * @access public
      * @return void
      */
@@ -312,6 +300,7 @@ class DbManager
 
     /**
      * 获得查询次数
+     * @deprecated
      * @access public
      * @return integer
      */
@@ -340,7 +329,7 @@ class DbManager
     {
         return $this->listen;
     }
-    
+
     /**
      * 获取所有连接实列
      * @access public
